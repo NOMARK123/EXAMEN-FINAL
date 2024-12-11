94aa75895c48145505fb8925005cb1aa5f44ca3a
NOMARK123 <markumbro9@gmail.com>
2024-12-10 22:48:42 -0500

    Update conversion_km_to_miles.py

--- conversion_km_to_miles.py
+++ conversion_km_to_miles.py
@@ -2,4 +2,4 @@
 For starting screen
 
 #Nps Install
-![Modelos de Texto (Text-based Models)](https://github.com/user-attachments/assets/a28214b0-cdb1-4b38-a4e5-f3ac8d83523c)
+![Modelos de Texto (Text-based Models)](https://github.com/user-attachments/assets/695121f3-a802-4443-b43d-3d4badb5f779)
