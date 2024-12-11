c9a99b48208923e55030656ccc6775b5dbf63acc
NOMARK123 <markumbro9@gmail.com>
2024-12-10 22:43:18 -0500

    Update conversion_km_to_miles.py

--- conversion_km_to_miles.py
+++ conversion_km_to_miles.py
@@ -1,5 +1,5 @@
 # Horizontal swiper
 For starting screen
 
-#Np Install
+#Nps Install
 ![Modelos de Texto (Text-based Models)](https://github.com/user-attachments/assets/a28214b0-cdb1-4b38-a4e5-f3ac8d83523c)
