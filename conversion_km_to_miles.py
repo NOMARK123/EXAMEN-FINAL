e9e65b20419e19599ec0c44bdbeb14792d4d69d3
NOMARK123 <markumbro9@gmail.com>
2024-12-10 22:17:37 -0500

    Create conversion_km_to_miles.py
    EJERCICIO 3

---
+++ conversion_km_to_miles.py
@@ -0,0 +1,8 @@
+git clone https://github.com/tu-usuario/conversion_km_to_miles.git
+cd conversion_km_to_miles
+
+git add conversion_km_to_miles.py
+
+git commit -m "Add neural network for KM to Miles conversion"
+
+git push origin main
